b12e682c18c584b3da0a6f3c1743420cac921cc1
Michelle Rogers <michelleevarogers@gmail.com>
2022-09-14 17:19:17 -0700

    add express boilerplate

---
+++ server/index.js
@@ -0,0 +1,26 @@
+// Require ENV
+
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// Express
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Middleware
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+// Listen
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on port: ${port} `);
+});
