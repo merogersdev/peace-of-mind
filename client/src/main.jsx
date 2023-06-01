241578232b8fd23f3491b7d8d04da0f8838fd3f4
Michelle Rogers <michelle@merogers.dev>
2023-06-01 18:31:43 -0500

    add node deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5000";
+import axios from 'axios';
+axios.defaults.baseURL = '/';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
--- server/server.js
+++ server/server.js
@@ -1,42 +1,60 @@
 // Require ENV
 
-require("dotenv").config();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // Route Imports
 
-const userRoutes = require("./routes/userRoutes");
-const entryRoutes = require("./routes/entryRoutes");
+const userRoutes = require('./routes/userRoutes');
+const entryRoutes = require('./routes/entryRoutes');
 
 // Express
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const helmet = require("helmet");
+const cors = require('cors');
+const helmet = require('helmet');
+const path = require('path');
 
 // Passport
 
-const passport = require("passport");
+const passport = require('passport');
 app.use(passport.initialize());
-require("./middleware/passport");
+require('./middleware/passport');
 
 app.use(express.json());
+
+// CORS
+const whitelist = [`${process.env.CLIENT_URL}`, 'http://localhost:5173'];
+
 app.use(
   cors({
-    origin: true,
-    credentials: true,
+    origin: whitelist,
   })
 );
 
 app.use(helmet());
 
-app.use("/users", userRoutes);
+app.use('/users', userRoutes);
 app.use(
-  "/entries",
-  passport.authenticate("jwt", { session: false }),
+  '/entries',
+  passport.authenticate('jwt', { session: false }),
   entryRoutes
 );
+
+// --- Serve Static Frontend Files - Node.js Deployment Only --- //
+if (process.env.NODE_DEPLOY === 'true') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (_req, res) =>
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (_req, res) => {
+    res.send('Peace of Mind API');
+  });
+}
 
 // Listen
 app.listen(port, () => {
