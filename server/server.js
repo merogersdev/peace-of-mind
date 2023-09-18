9a2b70323bff8e51cb03469a1322c5067ffdc678
Michelle Rogers <michelle@merogers.dev>
2023-09-18 16:22:55 -0500

    cleanup

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ const port = process.env.PORT || 5000;
 
 // Route Imports
 
-const userRoutes = require('./routes/userRoutes');
-const entryRoutes = require('./routes/entryRoutes');
+const indexRoutes = require('./routes');
 
 // Express
 
@@ -14,7 +13,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const helmet = require('helmet');
-const path = require('path');
 
 // Passport
 
@@ -22,39 +20,20 @@ const passport = require('passport');
 app.use(passport.initialize());
 require('./middleware/passport');
 
+// Middleware
+
 app.use(express.json());
-
-// CORS
-const whitelist = [`${process.env.CLIENT_URL}`, 'http://localhost:5173'];
-
-app.use(
-  cors({
-    origin: whitelist,
-  })
-);
-
+app.use(cors());
 app.use(helmet());
 
-app.use('/users', userRoutes);
-app.use(
-  '/entries',
-  passport.authenticate('jwt', { session: false }),
-  entryRoutes
+// Main Route
+
+app.use('/', indexRoutes);
+
+//  404 Catch
+app.use('*', (_req, res) =>
+  res.status(404).json({ message: 'Endpoint not found' })
 );
-
-// --- Serve Static Frontend Files - Node.js Deployment Only --- //
-if (process.env.NODE_DEPLOY === 'true') {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '../client/build')));
-
-  app.get('*', (_req, res) =>
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (_req, res) => {
-    res.send('Peace of Mind API');
-  });
-}
 
 // Listen
 app.listen(port, () => {
